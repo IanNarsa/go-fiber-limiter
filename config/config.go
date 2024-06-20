877d152a9218cae2ed7e8732cccbccd586a1126d
septian <septian.narsa>
2024-06-20 16:15:01 +0700

    call cred from env

--- config/config.go
+++ config/config.go
@@ -1,24 +1,34 @@
 package config
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/jmoiron/sqlx"
 )
 
 type Config struct {
-	DatabaseURL string
+	DB_HOST     string
+	DB_PORT     string
+	DB_USER     string
+	DB_PASSWORD string
+	DB_NAME     string
 }
 
 func LoadConfig() *Config {
 	return &Config{
-		DatabaseURL: os.Getenv("DATABASE_URL"),
+		DB_HOST:     os.Getenv("DB_HOST"),
+		DB_PORT:     os.Getenv("DB_PORT"),
+		DB_USER:     os.Getenv("DB_USER"),
+		DB_PASSWORD: os.Getenv("DB_PASSWORD"),
+		DB_NAME:     os.Getenv("DB_NAME"),
 	}
 }
 
 // ConnectDatabase establishes a connection to the database and returns the connection handle
 func ConnectDatabase(cfg *Config) (*sqlx.DB, error) {
-	db, err := sqlx.Connect("postgres", cfg.DatabaseURL)
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", cfg.DB_USER, cfg.DB_PASSWORD, cfg.DB_HOST, cfg.DB_PORT, cfg.DB_NAME)
+	db, err := sqlx.Connect("postgres", url)
 	if err != nil {
 		return nil, err
 	}
